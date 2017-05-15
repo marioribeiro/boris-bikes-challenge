a72dc188aa6cde9c888f00d62ce5a52310358a84
Mário Ribeiro <mail@marioribeiro.com>
2017-05-15 20:52:00 +0100

    Add dock method to DockingStation

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,10 +1,16 @@
 require 'docking_station'
 
-
 describe DockingStation do
-  it { is_expected.to respond_to :release_bike }
+
+  subject(:docking_station) { DockingStation.new }
+
+  it { is_expected.to respond_to(:release_bike) }
+  it { is_expected.to respond_to(:dock).with(1).argument }
+  it { is_expected.to respond_to(:bike) }
+
   it 'releases a bike' do
-    bike = subject.release_bike
+    bike = docking_station.release_bike
     expect(bike).to be_working
   end
+
 end
